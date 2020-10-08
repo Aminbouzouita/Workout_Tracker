32ad2401e203dd0f2951cd94888b736f49953358
Amin Bouzouita <Aminbouzouita@yahoo.com>
2020-10-07 19:46:20 -0700

    update models

--- models/workouts.js
+++ models/workouts.js
@@ -3,31 +3,47 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  name: {
-    type: String,
-    unique: true
-  },
-  distance: {
-    type: Number
-  },
-  duration: {
-    type: Number
-  },
-  cardio_name: {
-    type: String
-  },
-  weight: {
-    type: Number
-  },
-  sets: {
-    type: Number
-  },
-  reps: {
-    type: Number
-  },
-  resistance_duration: {
-    type: Number
-  },
+ day: [
+   {
+   type : Date, 
+   default: Date.now 
+  }
+],
+  exercises: [
+    {
+      type: {
+        type: String,
+        
+      },
+      name: {
+        type: 
+      },
+      duration: {
+        type: Number
+      },
+      
+      weight: {
+        type: Number,
+        required: false,
+        default: null,
+      },
+      reps: {
+        type: Number,
+        required: false,
+        default: null,
+      },
+      sets: {
+        type: Number,
+        required: false,
+        default: null,
+      },
+      distance: {
+        type: Number,
+        required: false,
+        default: null,
+      }
+    }]
+    
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- server.js
+++ server.js
@@ -1,34 +1,60 @@
 // Your assignment is to define the routes below. Good luck!
 const express = require("express");
-const mongojs = require("mongojs");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
 const app = express();
+
 app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
-const databaseUrl = "workout";
-const collections = ["exercices"];
-const db = mongojs(databaseUrl, collections);
-db.on("error", error => {
-  console.log("Database Error:", error);
-});
 
-// Find all exercises
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true,  useFindAndModify: false });
+
+// Find all Workouts
 app.get("/api/workouts", (req, res) => {
-  db.exercise.find({}, (error, found) => {
-    if (error) throw error;
-    res.status(200).json(found);
+  db.Workout.find({})
+  .then(function (dbUser) {
+    res.json(dbUser);
+  }).catch(function (err) {
+    res.json(err);
   });
 });
 
-//Create exercices
-app.post("/api/workouts"),(req,res) => {
-  db.exercise.create({body})
-  .then(Exercice => {
-    console.log(Exercice);
+//Create Exercice
+app.put("/api/workouts/:id",({exercices},res) => {
+  db.Workout.update({exercices})
+  .then(({ _id }, { $push: { exercices: _id } }, { new: true }))
+  .then(dbWorkout => {
+    res.json(dbWorkout);
   })
-  .catch(({ message }) => {
-    console.log(message);
+  .catch(err => {
+    res.json(err);
   });
-}
+})
+// 4. Update one note in the database's collection by it's ObjectId
+// (remember, mongojs.ObjectId(IdYouWantToFind)
+// POST: /api/workouts/:id
+// ================================================================
+// app.put("/api/workouts/:id", function({body}, res) {
+//   db.Workout.update({ name, type, weight, sets, reps, duration ,distance})
+//   .then(({_id:id}) => db.Workout.findOneAndUpdate({}, { $push: { exercices: _id } }, { new: true }))
+//    .then(function(err, data) {
+//       if (err) throw err;
+//       res.status(200).json(data);
+//     }
+//   );
+// });
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
