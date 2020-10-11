7a42718a5ae1cfd055257ef6f9039386d3b2cd41
Amin Bouzouita <Aminbouzouita@yahoo.com>
2020-10-10 22:16:21 -0700

    updating the create function

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-// Your assignment is to define the routes below. Good luck!
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 var path = require('path');
 const db = require("./models");
-
+const { type } = require("os");
+const { collection } = require("./models/workouts");
 const app = express();
-
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true,  useFindAndModify: false,  useUnifiedTopology: true });
-// app.get("/exercise", (req, res) => {
-//   res.sendFile(path.join(__dirname+'/public/index.html'));
-// });
+
+// On click new workout direct page to exercice.html page
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname+'/public/exercise.html'));
+});
+
+// On click continue workout direct page to stats.html page
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname+'/public/stats.html'));
+});
+
 // Find all Workouts
 app.get("/api/workouts", (req, res) => {
   db.Workout.find({})
@@ -30,11 +34,10 @@ app.get("/api/workouts", (req, res) => {
 });
 
 //Update Workout
-
 app.put("/api/workouts/:id",(req,res) => {
-  const {_id, name, type, weight, duration, reps, sets, distance} = req.body;
-  db.Workout.update(
-  ({}, { $push: { exercises: { name, type, weight, duration, reps, sets, distance}} }))
+  const data = req.body;
+  db.Workout.findByIdAndUpdate(
+  {_id :mongoose.Types.ObjectId(req.params.id)}, { $push: { exercises: data } } ) 
   .then(dbWorkout => {
     res.json(dbWorkout);
   })
@@ -43,15 +46,29 @@ app.put("/api/workouts/:id",(req,res) => {
   });
 })
 
-// app.post("/api/workouts", ({ body }, res) => {
-//   db.Workout.create(body)
-//     .then(data => {
-//       res.status(201).json(data);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
+//create new workout 
+app.post("/api/workouts", (req, res) => {
+const date = Date.now();
+const data = req.body;
+  db.Workout.create( {day: date})
+  .then(({_id}) => db.Workout.findOneAndUpdate({_id}, { $push: { exercises: data  } }))
+  .then(dbUser => {
+    res.json(dbUser);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+});
+
+// find workout by range
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+    .then(function (dbUser) {
+      res.json(dbUser);
+    }).catch(function (err) {
+      res.json(err);
+    });
+  });
 
 // Start the server
 app.listen(PORT, () => {
